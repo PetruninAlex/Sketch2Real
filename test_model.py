5079e868b36b27bc01f10d138ecb0b2226d24100
DeepLearning VM <google-dl-platform@googlegroups.com>
2019-08-07 13:49:29 +0000

    update

--- test_model.py
+++ test_model.py
@@ -1,7 +1,7 @@
 import argparse
 import os
 
-import scipy
+import imageio
 from PIL import Image
 
 from extract_edges import extract_edges
@@ -12,15 +12,16 @@ args = parser.parse_args()
 
 dir = args.dir
 list_of_files = os.listdir(dir)
+number_of_images = len(list_of_files)
 
 for file in list_of_files:
     img_path = dir + file
     img = Image.open(img_path).resize((224, 224))
     edges = extract_edges(img)
-    scipy.misc.imsave("./tmp/" + file, edges)
+    imageio.imwrite("./tmp/" + file, edges)
 
 os.system(
-    "python test.py --dataroot ./tmp --name best_model --model test --netG unet_256 --direction BtoA --dataset_mode single --norm batch")
+    "python test.py --dataroot ./tmp --name TODO --model test --netG unet_256 --direction BtoA --dataset_mode single --norm batch --num_test " + str(number_of_images))
 
 for file in list_of_files:
     os.system("rm ./tmp/" + file)
